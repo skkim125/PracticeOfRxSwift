4af46973b0d8260adebcc85e5ec4d17f1b7a9022
skkim125 <kthanks125@gmail.com>
2024-08-02 17:05:49 +0900

    PracticeOfRxSwift - PhoneViewController: phoneTextField의 text를 전화번호의 필수조건과 비교하여 ValidLabel text 표시 및 다음 버튼 활성화

--- PracticeOfRxSwift/SignUPOptional/PhoneViewController.swift
+++ PracticeOfRxSwift/SignUPOptional/PhoneViewController.swift
@@ -6,12 +6,18 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
 
 final class PhoneViewController: UIViewController {
-   
-    let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
-    let nextButton = PointButton(title: "다음")
+    private let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
+    private let phoneValidLabel = UILabel()
+    private let nextButton = PointButton(title: "다음")
+    
+    private var phoneValidText = PublishRelay<String>()
+    private var phoneText = BehaviorRelay(value: "010")
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +25,13 @@ final class PhoneViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        configureView()
+        bind()
     }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
-    }
-
     
     func configureLayout() {
         view.addSubview(phoneTextField)
+        view.addSubview(phoneValidLabel)
         view.addSubview(nextButton)
          
         phoneTextField.snp.makeConstraints { make in
@@ -38,11 +40,73 @@ final class PhoneViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
-        nextButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+        phoneValidLabel.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.top.equalTo(phoneTextField.snp.bottom).offset(5)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(phoneValidLabel.snp.bottom).offset(5)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+    }
+    
+    func configureView() {
+        phoneTextField.keyboardType = .phonePad
+    }
+    
+    func bind() {
+        phoneText
+            .bind(to: phoneTextField.rx.text)
+            .disposed(by: disposeBag)
+        
+        phoneTextField.rx.text.orEmpty
+            .bind(to: phoneValidText)
+            .disposed(by: disposeBag)
+        
+        phoneValidText
+            .bind(to: phoneValidLabel.rx.text)
+            .disposed(by: disposeBag)
+        
+        let phoneValidOfIsEmpty = phoneTextField.rx.text.orEmpty
+            .map({ !$0.trimmingCharacters(in: .whitespaces).isEmpty })
+        
+        let phoneValidOfNum = phoneTextField.rx.text.orEmpty
+            .map({ Int($0) != nil })
+        
+        let phoneValidOfCount = phoneTextField.rx.text.orEmpty
+            .map({ $0.trimmingCharacters(in: .whitespaces).count >= 10 })
+        
+        Observable.combineLatest(phoneValidOfIsEmpty, phoneValidOfNum, phoneValidOfCount)
+            .bind(with: self) { owner, isValid in
+                let allValid = isValid.0 && isValid.1 && isValid.2
+                let labelColor: UIColor = allValid ? .systemGreen : .systemGray
+                let buttonColor: UIColor = allValid ? .systemGreen : .systemGray
+
+                owner.phoneValidLabel.rx.isHidden.onNext(false)
+                owner.phoneValidLabel.rx.textColor.onNext(labelColor)
+                owner.nextButton.rx.backgroundColor.onNext(buttonColor)
+                owner.nextButton.rx.isEnabled.onNext(allValid)
+                
+                if isValid.0 && isValid.1 && isValid.2 == true {
+                    owner.phoneValidText.accept("올바른 형식입니다.")
+                } else {
+                    if !isValid.0 || !isValid.2 {
+                        owner.phoneValidText.accept("10자 이상 입력하세요.")
+                    } else if !isValid.1 {
+                        owner.phoneValidText.accept("숫자만 입력 가능합니다.")
+                    }
+                }
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(NicknameViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
     }
 
 }
