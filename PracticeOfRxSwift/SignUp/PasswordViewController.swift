5713b3f1a7c968a1eb90742e1848ad45d8a22c80
skkim125 <kthanks125@gmail.com>
2024-08-02 15:16:45 +0900

    PracticeOfRxSwift - PasswordViewController: passwordTextField의 text를 비밀번호 조건과 비교하여 ValidLabel 표시 및 다음 버튼 활성화

--- PracticeOfRxSwift/SignUp/PasswordViewController.swift
+++ PracticeOfRxSwift/SignUp/PasswordViewController.swift
@@ -6,12 +6,17 @@
 //
 
 import UIKit
+import RxSwift
+import RxCocoa
 import SnapKit
 
 final class PasswordViewController: UIViewController {
    
-    let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
-    let nextButton = PointButton(title: "다음")
+    private let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
+    private let passwordValidLabel = UILabel()
+    private let nextButton = PointButton(title: "다음")
+    
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +24,12 @@ final class PasswordViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-    }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+        bind()
     }
     
     func configureLayout() {
         view.addSubview(passwordTextField)
+        view.addSubview(passwordValidLabel)
         view.addSubview(nextButton)
          
         passwordTextField.snp.makeConstraints { make in
@@ -37,11 +38,42 @@ final class PasswordViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
-        nextButton.snp.makeConstraints { make in
-            make.height.equalTo(50)
-            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+        passwordValidLabel.snp.makeConstraints { make in
+            make.height.equalTo(30)
+            make.top.equalTo(passwordTextField.snp.bottom).offset(5)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
+        
+        nextButton.snp.makeConstraints { make in
+            make.height.equalTo(50)
+            make.top.equalTo(passwordValidLabel.snp.bottom).offset(5)
+            make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
+        }
+        
+        passwordValidLabel.font = .systemFont(ofSize: 14)
     }
-
+    
+    func bind() {
+        let passwordValid = passwordTextField.rx.text.orEmpty
+            .map({ $0.count >= 8 })
+        
+        passwordValid
+            .bind(with: self) { owner, isValid in
+                let labelColor: UIColor = isValid ? .black : .systemRed
+                let buttonColor: UIColor = isValid ? .systemGreen : .systemGray
+                let labelText = isValid ? "알맞은 형식입니다" : "8자 이상 입력해주세요"
+                owner.passwordValidLabel.rx.text.onNext(labelText)
+                owner.passwordValidLabel.rx.textColor.onNext(labelColor)
+                
+                owner.nextButton.rx.backgroundColor.onNext(buttonColor)
+                owner.nextButton.rx.isEnabled.onNext(isValid)
+            }
+            .disposed(by: disposeBag)
+        
+        nextButton.rx.tap
+            .bind(with: self) { owner, _ in
+                owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+            }
+            .disposed(by: disposeBag)
+    }
 }
